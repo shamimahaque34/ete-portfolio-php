8b327b119afad7694112f6b59b348d1412e52f14
Shamima Haque <haqueshamima34@gmail.com>
2024-08-02 20:05:26 +0600

    first commit

---
+++ frontend/pages/Frontend/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$ete = 'ete';
+
+echo $ete;
---
+++ frontend/pages/includes/header.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        html,body{
+            width: 100%;
+            height: 100%;
+            margin: 0px;
+            overflow-x: hidden;
+        }
+    </style>
+    
+    <!-- Required meta tags link Start Here -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Hello! I'm Shamima Haque. I'm a Web Developer">
+    <meta name="author" content="Shamima Haque">
+    <!-- Required meta tags link End Here -->
+
+    <!-- Bootstrap CSS link  Start Here-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap CSS link End Here -->
+    
+    <!--owl carousel css link Starts Here-->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.min.css">
+    <link rel="stylesheet" href="css/owl.transitions.min.css">
+    <!--owl carousel css link Ends Here-->
+    
+    
+    <!-- Fontawesome link Start Here   -->
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <!-- Fontawesome link End Here   -->
+
+    <!--    Favicon link Start Here-->
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+    <!--    Favicon link End Here-->
+
+    <!--Custom CSS link Start Here-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Custom CSS link End Here-->
+
+    <title>Shamima Haque</title>
+</head>
+
+<body onload="myFunction()">
+
+<!--Preloader Starts Here-->
+<div id="loading"></div>
+<!--Preloader Ends Here-->
+      
+<!--Header Starts Here-->
+<header>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand logo" href="index.html">
+        <img src="images/logo.png" alt="logo">
+<!--       <h2>Shamima Haque</h2>-->
+        </a>
+        
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.html">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services.html">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="portfolio.html">Portfolio</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="contact.html">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+</header>
+<!--Header Ends Here-->   
+
