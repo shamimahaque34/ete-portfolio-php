a48e80b1c4d52c8f651c95255d2a73a5d8d71704
Shamima Haque <haqueshamima34@gmail.com>
2024-08-03 16:55:47 +0600

    dashboard added

---
+++ backend/pages/includes/footer.php
@@ -0,0 +1,119 @@
+<!-- FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <div class="row align-items-center flex-row-reverse">
+            <div class="col-md-12 col-sm-12 text-center">
+                Copyright © {{ date('Y') }} <a href="#">Ete Portfolio</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="#"> Shamima Haque </a> All rights reserved
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- FOOTER CLOSED -->
+ <!-- JQUERY JS -->
+<script src="../../assets/plugins/jquery/jquery.min.js"></script>
+
+<!-- BOOTSTRAP JS -->
+<script src="../../assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- SIDE-MENU JS -->
+<script src="../../assets/plugins/sidemenu/sidemenu.js"></script>
+
+<!-- Perfect SCROLLBAR JS-->
+<script src="../../assets/plugins/p-scroll/perfect-scrollbar.js"></script>
+<script src="../../assets/plugins/p-scroll/pscroll.js"></script>
+
+<!-- STICKY JS -->
+<script src="../../assets/js/sticky.js"></script>
+
+
+<!-- APEXCHART JS -->
+<script src="../../assets/js/apexcharts.js"></script>
+
+<!-- INTERNAL SELECT2 JS -->
+<script src="../../assets/plugins/select2/select2.full.min.js"></script>
+
+<!-- CHART-CIRCLE JS-->
+<script src="../../assets/plugins/circle-progress/circle-progress.min.js"></script>
+
+<!-- INTERNAL DATA-TABLES JS-->
+<script src="../../assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+<script src="../../assets/plugins/datatable/js/dataTables.bootstrap5.js"></script>
+<script src="../../assets/plugins/datatable/dataTables.responsive.min.js"></script>
+
+<!-- DATA TABLE JS-->
+<script src="../../assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
+<script src="../../assets/plugins/datatable/js/buttons.bootstrap5.min.js"></script>
+<script src="../../assets/plugins/datatable/js/jszip.min.js"></script>
+<script src="../../assets/plugins/datatable/pdfmake/pdfmake.min.js"></script>
+<script src="../../assets/plugins/datatable/pdfmake/vfs_fonts.js"></script>
+<script src="../../assets/plugins/datatable/js/buttons.html5.min.js"></script>
+<script src="../../assets/plugins/datatable/js/buttons.print.min.js"></script>
+<script src="../../assets/plugins/datatable/js/buttons.colVis.min.js"></script>
+<script src="../../assets/plugins/datatable/responsive.bootstrap5.min.js"></script>
+<script src="../../assets/js/table-data.js"></script>
+
+<!-- jQuery UI Date Picker js -->
+<script src="../../assets/plugins/date-picker/jquery-ui.js"></script>
+
+<!-- bootstrap-datepicker js (Date picker Style-01) -->
+{{--<script src="../../assets/plugins/bootstrap-datepicker/js/datepicker.js"></script>--}}
+
+<!-- FORM ELEMENTS JS -->
+<script src="../../assets/js/formelementadvnced.js"></script>
+
+<!-- INDEX JS -->
+<script src="../../assets/js/index1.js"></script>
+<script src="../../assets/js/index.js"></script>
+
+<!-- Reply JS-->
+<script src="../../assets/js/reply.js"></script>
+
+
+<!-- COLOR THEME JS -->
+<script src="../../assets/js/themeColors.js"></script>
+
+<!-- CUSTOM JS -->
+<script src="../../assets/js/custom.js"></script>
+
+<!-- SWITCHER JS -->
+<script src="../../assets/switcher/js/switcher.js"></script>
+
+<!-- {{--Toastr JS--}} -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- {{--sweet alert2--}} -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- INTERNAL Summernote Editor js -->
+<script src="../../assets/plugins/summernote-editor/summernote1.js"></script>
+<script src="../../assets/js/summernote.js"></script>
+  
+  <!-- CK Editor -->
+<script src="//cdn.ckeditor.com/4.17.2/full/ckeditor.js"></script>
+
+<!-- <script>
+    $(function () {
+        $("#courseDate").datepicker();
+    })
+</script>
+<script>
+    $(document).on('click', '.delete-item', function () {
+        event.preventDefault();
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $(this).parent().submit();
+            }
+        });
+    });
+</script> -->
+
+
